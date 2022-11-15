f0cc88dc72445d97183790b2f403453d281ba4b3
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 16:13:01 +0100

    beginning of something

---
+++ srcs/parse.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 15:49:30 by OrioPrisc         #+#    #+#             */
+/*   Updated: 2022/11/15 16:10:49 by OrioPrisc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+const static char	*g_flags = ".0 #+-";
+const static char	*g_conversions = "\%cspdiuxX";
+
+void	find_parseable(char *str)
+{
+}
+
+// any of flags (no .) [[any number]+.[any number]+]any of flags(no .)
+void	parse(char *str, size_t n)
+{
+}
