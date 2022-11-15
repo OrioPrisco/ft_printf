31963751147748c7872e650392729664cf33aa2e
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 19:15:26 +0100

    beginning of parsing

--- srcs/parse.c
+++ srcs/parse.c
@@ -13,10 +13,24 @@
 #include <libft.h>
 
 const static char	*g_flags = ".0 #+-";
-const static char	*g_conversions = "\%cspdiuxX";
+const static char	*g_flags_num = ". #+-0123456789";
+const static char	*g_conversions = "cspdiuxX%";
 
 void	find_parseable(char *str)
 {
+	size_t	n;
+	char	*begin;
+
+	begin = strchrnul(str, '%');
+	write(1, str, begin - str);
+	if (!*begin)
+		returni ;
+	n = 1;
+	while (begin[n] && ft_strchr(begin[n], g_flags_num))
+		n++;
+	if (!ft_strchr(begin[n], g_conversions))
+		return ; //BAD FORMAT
+	parse(begin, n);
 }
 
 // any of flags (no .) [[any number]+.[any number]+]any of flags(no .)
