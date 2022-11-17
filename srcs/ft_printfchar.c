55e9f3a0e36b95ee889fe82199e7a9c90635b9b8
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-17 18:39:29 +0100

    printfchar, should be complete

---
+++ srcs/ft_printfchar.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printfchar.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 16:42:25 by OrioPrisc         #+#    #+#             */
+/*   Updated: 2022/11/17 18:35:03 by OrioPrisc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ftprintf.h"
+#include <stdarg.h>
+#include <libfth.h>
+
+ssize_t	ft_printfchar(t_flags flags, int precision, int width, va_list *ap)
+{
+	char	c;
+
+	c = va_arg(*ap, char);
+	(void)precision;
+	if (width > 1 && !(flags & FLAG_MINUS))
+		if (ft_pad(' ', width - 1) < 0)
+			return (-1);
+	if (write(1, &c, 1) < 0)
+		return (-1);
+	if (width > 1 && (flags & FLAG_MINUS))
+		if (ft_pad(' ', width - 1) < 0)
+			return (-1);
+	if (width > 1)
+		return (width);
+	return (1);
+}
