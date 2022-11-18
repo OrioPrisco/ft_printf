59862fa93c81e200dde21cfb53b987f65146ca88
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-18 17:59:08 +0100

    completed printfstr , seems to crash in some situations tho

--- srcs/ft_printfstr.c
+++ srcs/ft_printfstr.c
@@ -12,18 +12,26 @@
 
 #include "ftprintf.h"
 #include <stdarg.h>
-#include <libfth.h>
+#include <libft.h>
+#include <unistd.h>
 
-int	ft_printfstr(t_flags flags, int precision, int width, va_list *ap)
+ssize_t	ft_printfstr(t_flags flags, int precision, int width, va_list *ap)
 {
 	char	*str;
-	size_t	len;
+	ssize_t	len;
 
 	str = va_arg(*ap, char *);
 	if (precision >= 0)
 		len = ft_strnlen(str, precision);
 	else
-		len = strlen(str);
-	(void)flags;
-	return (0);
+		len = ft_strlen(str);
+	if (!(flags && FLAG_MINUS) && (len < width) && ft_pad(' ', width - len) < 0)
+		return (-1);
+	if (write(1, str, len) < 0)
+		return (-1);
+	if (flags && FLAG_MINUS && (len < width) && ft_pad(' ', width - len) < 0)
+		return (-1);
+	if (len < width)
+		return (width);
+	return (len);
 }
