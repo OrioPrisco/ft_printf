83abe62b04dd519e72f0be4d1cb0821af563e105
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-20 13:50:49 +0100

    Fix incorrect behaviour with digits precision
    Sign, spaces and the extra form are not part of the precision
    but rather part of th width of the number

--- srcs/ft_printfnum.c
+++ srcs/ft_printfnum.c
@@ -15,7 +15,7 @@
 #include <libft.h>
 
 //calculates size of the string that will be created by putnbr_base
-//includes the 0x for base 16 conversions
+//omits the 0x for base 16 conversions
 //and the eventual sign
 static int	ft_strlen_b(unsigned long nb, size_t base_len)
 {
@@ -25,7 +25,7 @@ static int	ft_strlen_b(unsigned long nb, size_t base_len)
 }
 
 //prints a number in a base to standard output
-static ssize_t	ft_putnbr_b(long nb, const char *base, size_t base_len)
+static ssize_t	ft_putnbr_b(unsigned long nb, const char *base, size_t base_len)
 {
 	if ((nb / base_len) > 0)
 		if (ft_putnbr_b(nb / base_len, base, base_len) < 0)
@@ -33,7 +33,11 @@ static ssize_t	ft_putnbr_b(long nb, const char *base, size_t base_len)
 	return (write(1, &base[nb % base_len], 1));
 }
 
-static int	ft_strlen_base(long nb, int base_len, int pws[3], t_flags flags)
+//calculates size of the string the extra stuff :
+//the 0x for base 16 conversions
+//and the eventual sign
+static int	ft_strlen_fluff(unsigned long nb,
+	int base_len, int pws[3], t_flags flags)
 {
 	int	size;
 
@@ -41,12 +45,18 @@ static int	ft_strlen_base(long nb, int base_len, int pws[3], t_flags flags)
 	if (pws[2] || (flags & (FLAG_PLUS | FLAG_SPACE)))
 		++size;
 	if (pws[0] > 0 || nb != 0)
-	{
 		if ((flags & FLAG_HASH) && base_len == 16 && nb != 0)
 			size += 2;
-		return (size + ft_strlen_b(nb, base_len));
-	}
 	return (size);
+}
+
+//returns the length a number should take
+//basically strlen_b with a special case for ) on precision 0
+static int	ft_strlen_base(unsigned long nb, int base_len, int precision)
+{
+	if (precision > 0 || nb != 0)
+		return (ft_strlen_b(nb, base_len));
+	return (0);
 }
 
 //number is a long, this way it can hold both int and uints
@@ -57,27 +67,29 @@ static int	ft_strlen_base(long nb, int base_len, int pws[3], t_flags flags)
 ssize_t	ft_printfnum(t_flags flags,
 		int pws[3], unsigned long number, const char *base)
 {
-	ssize_t	length;
+	ssize_t	digits;
 	int		base_len;
 	int		precision;
+	int		width;
 
 	base_len = ft_strlen(base);
-	length = ft_strlen_base(number, base_len, pws, flags);
-	precision = FT_MAX(pws[0], length);
-	if ((!(flags & (FLAG_MINUS | FLAG_ZERO)) && pws[1] > precision
-			&& ft_pad(' ', pws[1] - precision) < 0)
+	digits = ft_strlen_base(number, base_len, pws[0]);
+	precision = FT_MAX(pws[0], digits);
+	width = precision + ft_strlen_fluff(number, base_len, pws, flags);
+	if ((!(flags & (FLAG_MINUS | FLAG_ZERO)) && pws[1] > width
+			&& ft_pad(' ', pws[1] - width) < 0)
 		|| ((pws[2] || (flags & (FLAG_PLUS | FLAG_SPACE)))
 			&& write(1, &" +--"[pws[2] * 2 + ((flags & FLAG_PLUS) > 0)], 1) < 0)
 		|| (number && base_len == 16 && flags & FLAG_HASH
 			&& write(1, &base[17], 2) < 0)
-		|| ((flags & FLAG_ZERO && pws[1] > precision)//
-			&& ft_pad('0', pws[1] - precision) < 0)//
-		|| (precision > length
-			&& ft_pad('0', precision - length) < 0)
+		|| ((flags & FLAG_ZERO && pws[1] > width)
+			&& ft_pad('0', pws[1] - width) < 0)
+		|| (precision > digits
+			&& ft_pad('0', precision - digits) < 0)
 		|| (!(pws[0] == 0 && number == 0)
 			&& (ft_putnbr_b(number, base, base_len) < 0))
-		|| (flags & FLAG_MINUS && pws[1] > precision
-			&& ft_pad(' ', pws[1] - precision) < 0))
+		|| (flags & FLAG_MINUS && pws[1] > width
+			&& ft_pad(' ', pws[1] - width) < 0))
 		return (-1);
 	return (FT_MAX(pws[1], precision));
 }
